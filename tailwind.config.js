5a7e91e75bdc7e0bc42e65135263bb167a84b000
Jonatan Helander <Jonatanhelander@hotmail.com>
2024-09-04 21:07:22 +0200

    stylings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        bookman: ['Bookman Old Style', 'serif'],
+      },
+    },
   },
   plugins: [],
 }
